761609cfaebd50d080b9c80b04ab1d0d5b9b8114
ankush26 <ankushgupta2601@gmail.com>
2022-01-08 19:15:18 +0530

    404 not fond page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Cart from "./components/Cart";
 import Home from "./components/Home";
+import NotFound from "./components/NotFound";
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path='/cart' element={<Cart/>}/>
           <Route path='/' element={<Home/>}/>
+          <Route path='*' element={<NotFound/>}/>
         </Routes>
       </BrowserRouter>
     </div>
