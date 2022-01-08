a776c918298a84bd09a7bdd0b1ea270b8b8a3019
ankush26 <ankushgupta2601@gmail.com>
2022-01-08 19:04:36 +0530

    Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Cart from "./components/Cart";
+import Home from "./components/Home";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
