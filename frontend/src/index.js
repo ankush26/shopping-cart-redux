96ef666c79137a8a6cab51a92b9af8571720f784
ankush26 <ankushgupta2601@gmail.com>
2022-01-09 12:09:29 +0530

    products api - RTK Querey

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import productsApi, {
+  productApi,
+  useGetAllProductsQuery,
+} from "./features/productsApi";
+
+const store = configureStore({
+  reducer: {
+    [productApi.reducerPath]: productApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(productApi.middleware)
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
