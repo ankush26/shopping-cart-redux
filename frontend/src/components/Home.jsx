d7d4e7139e8e08b6eff8f89310e8720c42473c22
ankush26 <ankushgupta2601@gmail.com>
2022-01-10 11:04:18 +0530

    Home page design

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,13 +1,34 @@
-import React from 'react'
-import { useGetAllProductsQuery } from '../features/productsApi'
+import React from "react";
+import { useGetAllProductsQuery } from "../features/productsApi";
 
 function Home() {
-    const {data, error, isLoading} =useGetAllProductsQuery()
-    return (
-        <div>
-            Home
-        </div>
-    )
+  const { data, error, isLoading } = useGetAllProductsQuery();
+  return (
+    <div className="home-container">
+      {isLoading ? (
+        <p>Loading..</p>
+      ) : error ? (
+        <p>Something went wrong</p>
+      ) : (
+        <>
+          <h2>New Arrivals</h2>
+          <div className="products">
+            {data?.map((product) => (
+              <div key={product.id} className="product">
+                <h3>{product.name}</h3>
+                <img src={product.image} alt={product.name} />
+                <div className="details">
+                  <span>{product.desc}</span>
+                  <span className="price">${product.price}</span>
+                </div>
+                <button>Add To Cart</button>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
